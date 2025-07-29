be1092c0730e414bcef73222eea7a0dd862799e7
venkatJLK <venkatesan@jlk-tech.co.in>
2025-07-29 15:35:26 +0530

    minor changes

--- src/components/common/image/Images.ts
+++ src/components/common/image/Images.ts
@@ -4,6 +4,10 @@ import login_logo from "../../../assets/login_logo.png"
 import sidebar_logo from "../../../assets/sidebar_logo.png"
 import sidebar_small_logo from "../../../assets/sidebarsmall.svg";
 import sidebar_large_logo from "../../../assets/cms-logo-1.png";
+import report_card_1 from "../../../assets/report_1.svg";
+import report_card_2 from "../../../assets/report_2.svg";
+import report_card_3 from "../../../assets/report_3.svg";
+
 
 
 const Images = {
@@ -11,7 +15,10 @@ const Images = {
     login_logo,
     sidebar_logo,
     sidebar_small_logo,
-    sidebar_large_logo
+    sidebar_large_logo,
+    report_card_1,
+    report_card_2,
+    report_card_3
 };
 
 export default Images;
