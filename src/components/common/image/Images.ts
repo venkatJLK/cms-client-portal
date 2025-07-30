d1e7614473cf552c848c4cef3f66b7fe74e03ae4
Sridhar-jlk <sridhar.ramagandhi@jlk-tech.co.in>
2025-07-30 12:09:39 +0530

    Sidebar Icon Changes

--- src/components/common/image/Images.ts
+++ src/components/common/image/Images.ts
@@ -8,7 +8,17 @@ import report_card_1 from "../../../assets/report_1.svg";
 import report_card_2 from "../../../assets/report_2.svg";
 import report_card_3 from "../../../assets/report_3.svg";
 import login_form_logo from "../../../assets/form_logo.png"
-
+import reportImage1 from "../../../assets/CMS-report1.png"
+import reportImage2 from "../../../assets/CMS-report2.jpg"
+import reportImage3 from "../../../assets/CMS-report3.jpg"
+import active_profile from "../../../assets/activeProfile.png";
+import inactive_profile from "../../../assets/inactiveProfile.png";
+import active_report from "../../../assets/activeReport.png";
+import inactive_report from "../../../assets/inactiveReport.png";
+import active_appointment from "../../../assets/activeAppointment.png";
+import inactive_appointment from "../../../assets/inactiveAppointment.png";
+import active_doctor from "../../../assets/activeDoctor.png";
+import inactive_doctor from "../../../assets/inactiveDoctor.png";
 
 const Images = {
     login_banner,
@@ -19,7 +29,18 @@ const Images = {
     report_card_1,
     report_card_2,
     report_card_3,
-    login_form_logo
+    login_form_logo,
+    reportImage1,
+    reportImage2,
+    reportImage3,
+    active_profile,
+    active_report,
+    active_appointment,
+    active_doctor,
+    inactive_profile,
+    inactive_report,
+    inactive_appointment,
+    inactive_doctor
 };
 
 export default Images;
